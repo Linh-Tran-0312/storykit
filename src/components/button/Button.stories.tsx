0eeb9e1390942c87e6a76b93160ba5c26b49805a
linhtran <linhtran.atwork@gmail.com>
2024-08-09 18:00:51 +0700

    feat: add sample button story

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj, StoryFn } from '@storybook/react';
+import React from 'react';
+import Button from './Button';
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof Button> = {
+    component: Button,
+    title: 'Button Story',
+    argTypes: {
+        variant: {
+            options: ['primary', 'secondary']
+        },
+        children: {
+            options: ['Button']
+        }
+    },
+  };
+
+export default meta;
+
+export const PrimaryButton: StoryFn<typeof Button> = () => {
+    return(
+        <Button variant='primary'>Primary</Button>
+    )
+}
+export const Template: StoryObj<typeof Button> = {
+    args: {
+        variant: 'secondary',
+        children: 'Secondary'
+    }
+}
+
+
+
+  
---
+++ src/components/button/Button.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Button.css';
+
+type ButtonProps = {
+    variant: 'primary' | 'secondary';
+    children: React.ReactNode
+} 
+
+const Button = ({variant, children} : ButtonProps) => {
+    return <button className={`btn btn-default btn-${variant}`}>{children}</button>
+}
+export default Button;
