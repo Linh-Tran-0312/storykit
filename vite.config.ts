c819848dae9c9e599f2d638594437c1e184d5bf8
linhtran <linhtran.atwork@gmail.com>
2024-08-09 17:58:24 +0700

    feat: set up story kit project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Give vite the ability to resolve imports using TypeScript's path mapping, which is defined in tsconfig files
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    plugins: [react(),tsconfigPaths()],
+    // optimizeDeps: {
+    //     esbuildOptions: {
+    //       // Node.js global to browser globalThis
+    //       define: {
+    //         global: 'globalThis',
+    //       },
+    //     },
+    // },
+})
