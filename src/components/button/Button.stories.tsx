6db73d5d93dcba3c45249d00451bba31488706c0
linhtran <linhtran.atwork@gmail.com>
2024-10-15 18:06:33 +0700

    chore: add eslint configuration

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -4,32 +4,26 @@ import Button from './Button';
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Button> = {
-    component: Button,
-    title: 'Button Story',
-    argTypes: {
-        variant: {
-            options: ['primary', 'secondary', 'error']
-        },
-        children: {
-            options: ['Button']
-        }
+  component: Button,
+  title: 'Button Story',
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'error'],
     },
-  };
+    children: {
+      options: ['Button'],
+    },
+  },
+};
 
 export default meta;
 
 export const PrimaryButton: StoryFn<typeof Button> = () => {
-    return(
-        <Button variant='primary'>Primary</Button>
-    )
-}
+  return <Button variant='primary'>Primary</Button>;
+};
 export const Template: StoryObj<typeof Button> = {
-    args: {
-        variant: 'secondary',
-        children: 'Secondary'
-    }
-}
-
-
-
-  
+  args: {
+    variant: 'secondary',
+    children: 'Secondary',
+  },
+};
