4f3652e6e698e04531a463f40bafbb034fa1806f
linhtran <linhtran.atwork@gmail.com>
2024-10-28 15:45:20 +0700

    build: build lib

--- dist/esm/src/components/button/Button.js
+++ dist/esm/src/components/button/Button.js
@@ -1,8 +1,10 @@
+import { __rest } from '../../../_virtual/_tslib.js';
 import * as React from 'react';
 import './Button.css.js';
 
-const Button = ({ variant, children }) => {
-    return React.createElement("button", { className: `btn btn-default btn-${variant}` }, children);
+const Button = (_a) => {
+    var { variant, children } = _a, props = __rest(_a, ["variant", "children"]);
+    return (React.createElement("button", Object.assign({ className: `btn btn-default btn-${variant}` }, props), children));
 };
 
 export { Button as default };
