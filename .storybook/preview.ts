7d050ecf8e6ea8f376f85049376e58f241854a25
linhtran <linhtran.atwork@gmail.com>
2024-10-30 18:57:30 +0700

    chore: config storybook preview to display stories in order

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,1 +1,14 @@
-// To control the way stories are rendered and add global decorators and parameters
+// Replace your-framework with the framework you are using (e.g., react, vue3)
+import { Preview } from '@storybook/react-vite';
+ 
+const preview: Preview = {
+  parameters: {
+    options: {
+      storySort: {
+        order: ['Components','Custom Hooks'],
+      },
+    },
+  },
+};
+ 
+export default preview;
--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -5,7 +5,7 @@ import Button from './Button';
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Button> = {
   component: Button,
-  title: 'Button Story',
+  title: 'Components/Button',
   argTypes: {
     variant: {
       options: ['primary', 'secondary', 'error'],
