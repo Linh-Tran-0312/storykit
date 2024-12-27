2e3235083e60bee2e48a2c2b15327ee41e2cf87c
linhtran <linhtran.atwork@gmail.com>
2024-12-27 18:06:55 +0700

    chore: update storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,12 @@
 import { StorybookConfig } from '@storybook/react-vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 // main.js configuration docs: https://storybook.js.org/docs/api/main-config/main-config-features
 // @storybook/addon-essentials docs: https://storybook.js.org/addons/@storybook/addon-essentials
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
     addons: [
         '@storybook/addon-essentials',
+        '@storybook/theming',
+        '@storybook/manager-api'
         // '@storybook/addon-interactions',
         // 'storybook-addon-i18n',
       ],
@@ -15,6 +16,7 @@ const config: StorybookConfig = {
     },
     docs: {
         autodocs: false,
+        
     },
 };
  
---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Storykit',
+  brandUrl: 'https://github.com/Linh-Tran-0312/storykit',
+  brandTarget: '_self',
+});
