508c742ba00e31bd788ff1fa9222cbbe43ba4d5c
linhtran <linhtran.atwork@gmail.com>
2025-05-31 17:52:18 +0700

    feat: add accessibility section

---
+++ src/accessibility/utils/Inspector.tsx
@@ -0,0 +1,28 @@
+import React, { useState, FC } from "react";
+const visibleAttrs = ["id", "role"]
+export const Inspector: FC<{ children: React.ReactNode, getCode: Function, className?: string }>  = ({ children, getCode, className }) => {
+ 
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLElement;
+    if (!target.dataset.componentId) return;
+
+    const attrs = Array.from(target.attributes)
+      .filter(attr => attr.name.startsWith("aria-") || visibleAttrs.includes(attr.name))
+      .map(attr => `\n ${attr.name}="${attr.value}"`)
+      .join(" ");
+
+    const tag = target.tagName.toLowerCase();
+    const inner = target.innerHTML.trim();
+    const jsx = `<${tag}${attrs}>\n${inner}\n</${tag}>`;
+
+  
+    getCode(jsx);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
---
+++ src/accessibility/utils/Previewer.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;  
+const PreviewContainer = styled.div`
+  width: 30%;
+  height: 100%;
+  min-width: 300px;
+  padding: 1rem;
+  background-color:rgb(240, 240, 240);
+  border: 1px solid #ddd;
+`;
+
+export const Previewer: FC<{ children: React.ReactNode, code: string }> = ({ children, code }) => {
+  return (
+    <Container>
+      <PreviewContainer>
+        <pre>{code}</pre>
+      </PreviewContainer>
+      <div className="pa-4">
+      {children}
+      </div>
+     
+    </Container>
+  );
+};
